c0119d010ceb512f7c8cafa634d60c053e59c5e6
Sarthak Jain <sarthak2107597@st.jmi.ac.in>
2023-08-06 02:47:16 +0530

    added user list scraping from search

--- automateMessages.py
+++ automateMessages.py
@@ -21,7 +21,7 @@ message = "this is an automated message"
 
 class instaBot:
 
-    def __init__(self, username, password, audience, message):
+    def __init__(self, username, password,target, audience, message):
 
         self.username = username
 
@@ -30,6 +30,8 @@ class instaBot:
         self.user = audience
         
         self.message = message
+
+        self.targetAudience = target
 
         self.base_url = 'https://www.instagram.com/'
 
@@ -48,6 +50,9 @@ class instaBot:
 
         enter_password.send_keys(Keys.RETURN)
         time.sleep(5)
+
+        self.accountList()
+        time.sleep(2)
 
         # to close popup
         self.bot.find_element("xpath",'/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/div').click()
@@ -92,8 +97,26 @@ class instaBot:
             self.bot.find_element("xpath",'/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/div/div/div/div[1]/div/div[1]/div/div[1]/div[2]/div/div').click()
             time.sleep(3)
 
+    def accountList(self):
+        # to click on the search user icon
+        self.bot.find_element("xpath",'/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[2]/span/div/a').click()
+        time.sleep(2)
+
+        #to search on the search bar
+        search_input = self.bot.find_element("xpath",'/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div/div/div[1]/div/div/input')
+        search_input.send_keys(self.targetAudience)
+        search_input.send_keys(Keys.RETURN)
+        time.sleep(5)
+        # to get the list of all the accouts
+        account_conatiner = self.bot.find_element("xpath",'/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div/div/div[2]/div')
+        account_list = account_conatiner.find_elements(By.CSS_SELECTOR,'span.x1s6885')
+        print(account_list, "this is the full account list")
+        # time.sleep(2)
+        # new_list = [account_item for index,account_item in enumerate(account_list) if index % 3 ==0]
+        # print(new_list, "this is the account list")
+
 def init():
-    instaBot("sarthak0jain@gmail.com","Sarthak@0909",audience,message)
+    instaBot("sarthak0jain@gmail.com","Sarthak@0909","marketing",audience,message)
     input("done")
 
 init()
