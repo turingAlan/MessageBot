76e12083a9072e5dca6967987f5ff526739fc6b0
Sarthak Jain <sarthak2107597@st.jmi.ac.in>
2023-08-06 16:36:13 +0530

    changed the ui

--- ui.py
+++ ui.py
@@ -2,10 +2,22 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
-frm = ttk.Frame(root,padding  = 30)
-root.title("My first python GUI")
-frm.grid()
-ttk.Label(frm,text = "Message").grid(column = 0,row = 0)
-ttk.Button(frm,text="quit",command = quit).grid(column = 0,row = 1)
+screen_height = root.winfo_screenheight()
+screen_width = root.winfo_screenwidth()
+
+# set the window size to half the screen size
+root.geometry(str(screen_width//2)+"x"+str(screen_height//2))
+root.title("InstaBot")
+
+title_label = ttk.Label(master=root,text= "InstaBot", font="Courier 30 bold")
+title_label.pack()
+
+
+# root.grid()
+test = ttk.Label(root,text = "Message")
+test.pack(side="left")
+exit = ttk.Button(root,text="quit",command = quit)
+exit.pack(side="right")
+
 
 root.mainloop()
